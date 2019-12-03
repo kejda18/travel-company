e36854239d8e9c880dc5a19c5240e19c7d5dc770
Kejda <kejdaorgocka18@gmail.com>
2019-12-03 11:28:35 +0100

    ...

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
+let revealOnScroll = new revealOnScroll();
 
 let mobileMenu = new MobileMenu();
 
